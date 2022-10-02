b4600ac65167282892f87dd0d67dfe1968b6600d
RahulGaur998 <gaur.rahul996@gmail.com>
2022-10-03 00:08:35 +0530

    ADD: added logic to test happy or sad mood according to inputs

--- src/test/java/com/moodanalyzer/AppTest.java
+++ src/test/java/com/moodanalyzer/AppTest.java
@@ -2,12 +2,40 @@ package com.moodanalyzer;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
 public class AppTest {
-    @Test
-    public void checkFirstName() {
-        MoodAnalyzer moodAnalyzerObject = new MoodAnalyzer();
-        assertEquals("SAD", moodAnalyzerObject.analyseMood("I am in Sad Mood"));
+    String userInput;
+    String expectation;
+
+    MoodAnalyzer moodAnalyzerObject;
+
+    @Before
+    public void initialize() {
+        moodAnalyzerObject = new MoodAnalyzer();
     }
+
+    public AppTest(String userInput, String expectation) {
+        this.userInput = userInput;
+        this.expectation = expectation;
+    }
+
+    @Parameterized.Parameters
+    public static Collection inputs() {
+        return Arrays.asList(new Object[][] { { "I am in sad mood", "SAD" }, { "I am in happy mood", "HAPPY" }
+        });
+    }
+
+    @Test
+    public void checkUserInput() {
+        assertEquals(this.expectation, moodAnalyzerObject.analyseMood(this.userInput));
+    }
+
 }
