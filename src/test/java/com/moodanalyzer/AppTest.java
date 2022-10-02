3eb07921fbf3959302bc624c8bf2588844ce3da2
RahulGaur998 <gaur.rahul996@gmail.com>
2022-10-03 00:35:02 +0530

    ADD: added Testing code for constructor to return happy if in happy mood

--- src/test/java/com/moodanalyzer/AppTest.java
+++ src/test/java/com/moodanalyzer/AppTest.java
@@ -42,10 +42,12 @@ public class AppTest {
     @Test
     public void constructerMoodAnalyzer() {
         String inputMessage = "I am in sad mood";
-
+        String inputMessage1 = "I am in happy mood";
         MoodAnalyzer moodAnalyzerObject = new MoodAnalyzer(inputMessage);
+        MoodAnalyzer moodAnalyzerObject1 = new MoodAnalyzer(inputMessage1);
         String result = moodAnalyzerObject.analyseMood();
-
+        String result1 = moodAnalyzerObject1.analyseMood();
         assertEquals("SAD", result);
+        assertEquals("HAPPY", result1);
     }
 }
